3632cac1a97c174250f805b63f584ab6532fdc64
Abhishek Kumar <abhishek.kum1908@gmail.com>
2023-07-30 01:28:30 +0530

    Added routes

--- main.go
+++ main.go
@@ -1,2 +1,34 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/19abhishek/e-commerce/controllers"
+	"github.com/19abhishek/e-commerce/database"
+	"github.com/19abhishek/e-commerce/middleware"
+	"github.com/19abhishek/e-commerce/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == ""{
+		port = "8080"
+	}
+
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddTCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/19abhishek/e-commerce/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/product", controllers.ProductViewAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
+}
