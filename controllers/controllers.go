8c73ad59f851e735ee23e2130326ac2c6f667bea
Abhishek Kumar <abhishek.kum1908@gmail.com>
2023-07-31 01:54:17 +0530

    L6 half

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,12 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/19abhishek/e-commerce/models"
+	"github.com/gin-gonic/gin"
+)
 
 func HashPassword (password string) string {
 
@@ -8,7 +14,29 @@ func HashPassword (password string) string {
 
 func VerifyPassword (userPassword string, givenPassword string) (bool, string) {}
 
-func SignUp() gin.HandlerFunc {}
+func SignUp() gin.HandlerFunc {
+
+	return func(c *gin.Context){
+		var ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var cancel models.User
+		if err  := c.BindJSON(&user); err != nil {
+			c.JSON{http.StatusBadRequest, gin.H("error": err.Error())}
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+		}
+
+		count, err != UserCollection.CountDocuments(ctx, bson.M{"error": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error"})
+		}
+	}
+}
 
 func Login() gin.HandlerFunc {}
 
--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,30 @@
 package database
+
+import "errors"
+
+var (
+
+	ErrCantFindProduct = errors.New("Can't find Product")
+	ErrCantDecodeProducts = errors.New("Can't Decode Products")
+	ErrUserIdIsNotValid = errors.New("User ID is not valid")
+	ErrCantUpdateUser = errors.New("Can't Update User")
+	ErrCantRemoveItemCart = errors.New("Can't Remove Item from cart")
+	ErrCantGetItem = errors.New("Can't Get Item from the cart")
+	ErrCantBuyCartItem = errors.New("Can't Buy Item from cart")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
